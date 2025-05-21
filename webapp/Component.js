bf12d8d406ce434a837fb46f03ee8ba8ad1aae4f
O96212 this-default-was-not-configured.invalid <o96212>
2025-05-21 11:31:15 +0000

    Initial Commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("zmdgchmaint.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
